77abdf22cea61935e16266e4347dac8cc21704bd
DynastyKids(づ｡◕‿‿◕｡)づ <shabibox@outlook.com>
2022-04-06 01:34:55 +1000

    Structure change. Data loaded in main.js, OSM only run render maps. Adding popup when fuel type selected

--- src/components/body/main.js
+++ src/components/body/main.js
@@ -4,36 +4,90 @@ import OnGoogleMap from "./googlemap";
 import OnOSMmap from './openstreetmap';
 
 export default function MainBody(props){
-    const pathname=window.location.pathname;
-    // console.log(pathname);
+    const hashpram=window.location.hash;
+    
+    const [isLoading, setLoading] = React.useState(true);
+    const [data, setData] = React.useState([]);
+    const getServoJson = async ()=>{
+        try {
+            const response = await fetch('https://fuel.danisty8.com/fuel');
+            const json = await response.json();
 
-    const [map, setMap] = useState(null);
-
-    if(pathname === "U94"){
-        const url="https://fuel.danisty8.com/E10";
-    } else if(pathname === "U95"){
-        const url="https://fuel.danisty8.com/P95";
-    } else if(pathname === "U98"){
-        const url="https://fuel.danisty8.com/P98";
-    } else if(pathname === "DL"){
-        const url="https://fuel.danisty8.com/DL";
-    } else if(pathname === "PDL"){
-        const url="https://fuel.danisty8.com/PDL";
-    } else if(pathname === "LPG"){
-        const url="https://fuel.danisty8.com/LPG";
-    } else if(pathname === "ADB"){
-        const url="https://fuel.danisty8.com/Adblue";
-        // Adblue only showing locations
-    } else{
-        const url="https://fuel.danisty8.com/U91";
-        // Path is not legal, use default U91
+            // Merge data into one large array
+            var mergedArray = []
+            json.NSW.forEach(element => {
+                mergedArray.push(element)
+            });
+            json.TAS.forEach(element => {
+                mergedArray.push(element)
+            });
+            json.WA.forEach(element => {
+                mergedArray.push(element)
+            });
+            if(hashpram === "#U91"){
+                var newElement = []
+                mergedArray.forEach(element => {
+                    if(element.U91 !== null){ newElement.push({"U91":element.U91,"address":element.address,"brand":element.brand,"loc_lat":element.loc_lat,"loc_lng":element.loc_lng,"name":element.name})}
+                });
+                setData(newElement);
+            }else if(hashpram === "#E10"){
+                var newElement = []
+                mergedArray.forEach(element => {
+                    if(element.E10 !== null){ newElement.push({"E10":element.E10,"address":element.address,"brand":element.brand,"loc_lat":element.loc_lat,"loc_lng":element.loc_lng,"name":element.name})}
+                });
+                setData(newElement);
+            } else if(hashpram === "#P95"){
+                var newElement = []
+                mergedArray.forEach(element => {
+                    if(element.P95 !== null){ newElement.push({"P95":element.P95,"address":element.address,"brand":element.brand,"loc_lat":element.loc_lat,"loc_lng":element.loc_lng,"name":element.name})}
+                });
+                setData(newElement);
+            } else if(hashpram === "#P98"){
+                var newElement = []
+                mergedArray.forEach(element => {
+                    if(element.P98 !== null){ newElement.push({"P98":element.P98,"address":element.address,"brand":element.brand,"loc_lat":element.loc_lat,"loc_lng":element.loc_lng,"name":element.name})}
+                });
+                setData(newElement);
+            } else if(hashpram === "#DL"){
+                var newElement = []
+                mergedArray.forEach(element => {
+                    if(element.DL !== null){ newElement.push({"DL":element.DL,"address":element.address,"brand":element.brand,"loc_lat":element.loc_lat,"loc_lng":element.loc_lng,"name":element.name})}
+                });
+                setData(newElement);
+            } else if(hashpram === "#PDL"){
+                var newElement = []
+                mergedArray.forEach(element => {
+                    if(element.PDL !== null){ newElement.push({"PDL":element.PDL,"address":element.address,"brand":element.brand,"loc_lat":element.loc_lat,"loc_lng":element.loc_lng,"name":element.name})}
+                });
+                setData(newElement);
+            } else if(hashpram === "#LPG"){
+                var newElement = []
+                mergedArray.forEach(element => {
+                    if(element.LPG !== null){ newElement.push({"LPG":element.LPG,"address":element.address,"brand":element.brand,"loc_lat":element.loc_lat,"loc_lng":element.loc_lng,"name":element.name})}
+                });
+                setData(newElement);
+            } else if(hashpram === "#ADB"){
+                // const url="https://fuel.danisty8.com/Adblue";
+                // Adblue only showing locations
+            } else {
+                setData(mergedArray);
+            }
+        } catch (error) {
+            console.error(error)
+        } finally{
+            setLoading(false);
+        }
     }
 
-    // console.log(props)
+    React.useEffect( ()=>{
+        getServoJson();
+        window.addEventListener("hashchange", getServoJson());
+    },[]);
+
     return (
         <>
             {/* <OnGoogleMap/> */}
-            <OnOSMmap/>
+            <OnOSMmap jsondata={data.length!==0 ? data:[]}/>
         </>
     );
 }
--- src/components/body/openstreetmap.js
+++ src/components/body/openstreetmap.js
@@ -1,254 +1,219 @@
-import { useCallback, useMemo, useState, useEffect  } from 'react'
+import * as React from 'react';
 // import {useLocation} from "react-router-dom";
-import {MapContainer, Rectangle,TileLayer,useMap,useMapEvent, Marker, Popup,ScaleControl } from 'react-leaflet'
+import { MapContainer, Rectangle, TileLayer, useMap, useMapEvent, Marker, Popup, ScaleControl, Tooltip } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 
 import 'react-leaflet-markercluster/dist/styles.min.css';
 import styles from "./osm_location-button.module.css";
 import L from "leaflet";
 import tileLayer from "./osm_tileLayer";
-export default function OnOSMmap() {
-    const [map, setMap] = useState(null);
 
-    const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
+export default function OnOSMmap(props) {
+    const hashpram=window.location.hash;
+    const [map, setMap] = React.useState(null);
 
-    // const fuelType = new URLSearchParams(useLocation().search).get('fuel')
-    // console.log(useLocation().search)
+    const [data, setData] = React.useState([]);
+    const [filteredArray, setFilteredArray] = React.useState([]);
 
-    const getServos = async () => {
-       try {
-        const response = await fetch('https://fuel.danisty8.com/fuel');
-        const json = await response.json();
-        let dataarray=[];
-        json.NSW.forEach(element => {
-          let pricetag=''
-          if(element.U91 != null){pricetag+= 'Unleaded 91: ' + element.U91}
-          if(element.E10 != null){pricetag+= '\nEthanol 10:'+ element.E10}
-          if(element.P95 != null){pricetag+= '\nPremium Unleaded 95: '+ element.P95}
-          if(element.P98 != null){pricetag+= '\nPremium Unleaded 98: '+ element.P98}
-          pricetag+='\n';
-          if(element.DL != null){pricetag+= '\nDiesel: '+ element.DL}
-          if(element.PDL != null){pricetag+= '\nPremium Diesel: '+ element.PDL}
-          if(element.B20 != null){pricetag+= '\nBioDiesel 20: '+ element.B20}
-          pricetag+='\n'
-          if(element.LPG != null){pricetag+= '\nLPG: '+ element.LPG}
-          dataarray.push({title:element.name,lat:element.loc_lat,lng:element.loc_lng,address:element.address,priceInfo:pricetag})
-        });
-        json.WA.forEach(element => {
-          let pricetag=''
-          if(element.U91 != null){pricetag+= 'Unleaded 91: ' + element.U91}
-          // if(element.E10 != null){pricetag+= '\nEthanol 10:'+ element.E10}
-          if(element.P95 != null){pricetag+= '\nPremium Unleaded 95: '+ element.P95}
-          if(element.P98 != null){pricetag+= '\nPremium Unleaded 98: '+ element.P98}
-          pricetag+='\n';
-          if(element.DL != null){pricetag+= '\nDiesel: '+ element.DL}
-          if(element.PDL != null){pricetag+= '\nPremium Diesel: '+ element.PDL}
-          if(element.B20 != null){pricetag+= '\nBioDiesel 20: '+ element.B20}
-          pricetag+='\n'
-          if(element.LPG != null){pricetag+= '\nLPG: '+ element.LPG}
-          dataarray.push({title:element.name,lat:element.loc_lat,lng:element.loc_lng,address:element.address,priceInfo:pricetag})
-        });
-        json.TAS.forEach(element => {
-          let pricetag=''
-          if(element.U91 != null){pricetag+= 'Unleaded 91: ' + element.U91}
-          if(element.E10 != null){pricetag+= '\nEthanol 10:'+ element.E10}
-          if(element.P95 != null){pricetag+= '\nPremium Unleaded 95: '+ element.P95}
-          if(element.P98 != null){pricetag+= '\nPremium Unleaded 98: '+ element.P98}
-          pricetag+='\n';
-          if(element.DL != null){pricetag+= '\nDiesel: '+ element.DL}
-          if(element.PDL != null){pricetag+= '\nPremium Diesel: '+ element.PDL}
-          if(element.B20 != null){pricetag+= '\nBioDiesel 20: '+ element.B20}
-          pricetag+='\n'
-          if(element.LPG != null){pricetag+= '\nLPG: '+ element.LPG}
-          dataarray.push({title:element.name,lat:element.loc_lat,lng:element.loc_lng,address:element.address,priceInfo:pricetag})
-        });
-        setData(dataarray);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-  
-    useEffect(() => {
-      getServos();
-    }, []);
+    React.useEffect(() => {
+        setData(props.jsondata);
+        setFilteredArray(DataBuilder(data))
+    }, [props,window.location.hash]);
 
-
-    const DisplayLineBreak ={
+    const DisplayLineBreak = {
         whiteSpace: "pre-line"
-      
     }
 
-  return (
-    <MapContainer center={[-28.0,133.0]} zoom={5}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    return (
+        <>
+            <MapContainer center={[-28.0, 133.0]} zoom={5}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
 
+                <MarkerClusterGroup>
+                    {filteredArray.length > 0 ? filteredArray.map(({ brand, title, lat, lng, address, priceInfo,price1 }, index) => (
+                        <Marker key={index} position={[lat, lng]}>
+                            {hashpram.length > 0 ? <Tooltip direction="top" opacity={1} permanent>{price1}</Tooltip>:<></>}
+                            <Popup><h5>{title}</h5>
+                                <p><b>Addr:</b> {address}</p>
+                                <div style={DisplayLineBreak}>
+                                    {priceInfo}
+                                </div>
+                            </Popup>
+                        </Marker>
+                    )) : <></>}
+                </MarkerClusterGroup>
 
-        <MarkerClusterGroup>
-        {data.map(({ title, lat, lng, address, priceInfo}, index) => (
-          <Marker key={index} position={[lat, lng]}>
-            <Popup><h5>{title}</h5>
-              <p><b>Addr:</b> {address}</p>
-                <div style={DisplayLineBreak}>
-                    {priceInfo}
-                </div>
-            </Popup>
-          </Marker>
-        ))}
-        </MarkerClusterGroup>
-
-      <LocationButton map={map} />
-      <ScaleControl imperial={false} />
-    </MapContainer>
-  )
+                <LocationButton map={map} />
+                <ScaleControl imperial={false} />
+            </MapContainer>
+        </>
+    )
 }
 
 
 // Location Button
-
 const LocationButton = () => {
     const map = useMap();
-  
-    useEffect(() => {
-      // create custom button
-      const customControl = L.Control.extend({
-        // button position
-        options: {
-          position: "topleft",
-          className: `${styles.locateButton} leaflet-bar`,
-          html: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3A8.994 8.994 0 0 0 13 3.06V1h-2v2.06A8.994 8.994 0 0 0 3.06 11H1v2h2.06A8.994 8.994 0 0 0 11 20.94V23h2v-2.06A8.994 8.994 0 0 0 20.94 13H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>',
-          style:
-            "width: 34px; height: 34px; left: 0; margin-top: 0; display: flex; cursor: pointer; justify-content: center; font-size: 2rem;",
-        },
-  
-        // method
-        onAdd: function (map) {
-          this._map = map;
-          const button = L.DomUtil.create("div");
-          L.DomEvent.disableClickPropagation(button);
-  
-          button.title = "locate";
-          button.innerHTML = this.options.html;
-          button.className = this.options.className;
-          button.setAttribute("style", this.options.style);
-  
-          L.DomEvent.on(button, "click", this._clicked, this);
-  
-          return button;
-        },
-        _clicked: function (e) {
-          L.DomEvent.stopPropagation(e);
-  
-          this._checkLocate();
-  
-          return;
-        },
-        _checkLocate: function () {
-          return this._locateMap();
-        },
-  
-        _locateMap: function () {
-          const locateActive = document.querySelector(`.${styles.locateButton}`);
-          const locate = locateActive.classList.contains(styles.locateActive);
-          // add/remove class from locate button
-          locateActive.classList[locate ? "remove" : "add"](styles.locateActive);
-  
-          // remove class from button
-          // and stop watching location
-          if (locate) {
-            this.removeLocate();
-            this._map.stopLocate();
-            return;
-          }
-  
-          // location on found
-          this._map.on("locationfound", this.onLocationFound, this);
-          // locataion on error
-          this._map.on("locationerror", this.onLocationError, this);
-  
-          // start locate
-          this._map.locate({ setView: true, enableHighAccuracy: true });
-        },
-        onLocationFound: function (e) {
-          // add circle
-          this.addCircle(e).addTo(this.featureGroup()).addTo(map);
-  
-          // add marker
-          this.addMarker(e).addTo(this.featureGroup()).addTo(map);
-  
-          // add legend
-        },
-        // on location error
-        onLocationError: function (e) {
-          this.addLegend("Location access denied.");
-        },
-        // feature group
-        featureGroup: function () {
-          return new L.FeatureGroup();
-        },
-        // add legend
-        addLegend: function (text) {
-          const checkIfDescriotnExist = document.querySelector(".description");
-  
-          if (checkIfDescriotnExist) {
-            checkIfDescriotnExist.textContent = text;
-            return;
-          }
-  
-          const legend = L.control({ position: "bottomleft" });
-  
-          legend.onAdd = function () {
-            let div = L.DomUtil.create("div", "description");
-            L.DomEvent.disableClickPropagation(div);
-            const textInfo = text;
-            div.insertAdjacentHTML("beforeend", textInfo);
-            return div;
-          };
-          legend.addTo(this._map);
-        },
-        addCircle: function ({ accuracy, latitude, longitude }) {
-          return L.circle([latitude, longitude], accuracy / 2, {
-            className: "circle-test",
-            weight: 2,
-            stroke: false,
-            fillColor: "#136aec",
-            fillOpacity: 0.15,
-          });
-        },
-        addMarker: function ({ latitude, longitude }) {
-          return L.marker([latitude, longitude], {
-            icon: L.divIcon({
-              className: styles.locatedAnimation,
-              iconSize: L.point(17, 17),
-              popupAnchor: [0, -15],
-            }),
-          }).bindPopup("Your are here :)");
-        },
-        removeLocate: function () {
-          this._map.eachLayer(function (layer) {
-            if (layer instanceof L.Marker) {
-              const { icon } = layer.options;
-              if (icon?.options.className === styles.locatedAnimation) {
-                map.removeLayer(layer);
-              }
-            }
-            if (layer instanceof L.Circle) {
-              if (layer.options.className === "circle-test") {
-                map.removeLayer(layer);
-              }
-            }
-          });
-        },
-      });
-  
-      // adding new button to map controll
-      map.addControl(new customControl());
+
+    React.useEffect(() => {
+        // create custom button
+        const customControl = L.Control.extend({
+            // button position
+            options: {
+                position: "topleft",
+                className: `${styles.locateButton} leaflet-bar`,
+                html: '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3A8.994 8.994 0 0 0 13 3.06V1h-2v2.06A8.994 8.994 0 0 0 3.06 11H1v2h2.06A8.994 8.994 0 0 0 11 20.94V23h2v-2.06A8.994 8.994 0 0 0 20.94 13H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>',
+                style:
+                    "width: 34px; height: 34px; left: 0; margin-top: 0; display: flex; cursor: pointer; justify-content: center; font-size: 2rem;",
+            },
+
+            // method
+            onAdd: function (map) {
+                this._map = map;
+                const button = L.DomUtil.create("div");
+                L.DomEvent.disableClickPropagation(button);
+
+                button.title = "locate";
+                button.innerHTML = this.options.html;
+                button.className = this.options.className;
+                button.setAttribute("style", this.options.style);
+
+                L.DomEvent.on(button, "click", this._clicked, this);
+
+                return button;
+            },
+            _clicked: function (e) {
+                L.DomEvent.stopPropagation(e);
+
+                this._checkLocate();
+
+                return;
+            },
+            _checkLocate: function () {
+                return this._locateMap();
+            },
+
+            _locateMap: function () {
+                const locateActive = document.querySelector(`.${styles.locateButton}`);
+                const locate = locateActive.classList.contains(styles.locateActive);
+                // add/remove class from locate button
+                locateActive.classList[locate ? "remove" : "add"](styles.locateActive);
+
+                // remove class from button
+                // and stop watching location
+                if (locate) {
+                    this.removeLocate();
+                    this._map.stopLocate();
+                    return;
+                }
+
+                // location on found
+                this._map.on("locationfound", this.onLocationFound, this);
+                // locataion on error
+                this._map.on("locationerror", this.onLocationError, this);
+
+                // start locate
+                this._map.locate({ setView: true, enableHighAccuracy: true });
+            },
+            onLocationFound: function (e) {
+                // add circle
+                this.addCircle(e).addTo(this.featureGroup()).addTo(map);
+
+                // add marker
+                this.addMarker(e).addTo(this.featureGroup()).addTo(map);
+
+                // add legend
+            },
+            // on location error
+            onLocationError: function (e) {
+                this.addLegend("Location access denied.");
+            },
+            // feature group
+            featureGroup: function () {
+                return new L.FeatureGroup();
+            },
+            // add legend
+            addLegend: function (text) {
+                const checkIfDescriotnExist = document.querySelector(".description");
+
+                if (checkIfDescriotnExist) {
+                    checkIfDescriotnExist.textContent = text;
+                    return;
+                }
+
+                const legend = L.control({ position: "bottomleft" });
+
+                legend.onAdd = function () {
+                    let div = L.DomUtil.create("div", "description");
+                    L.DomEvent.disableClickPropagation(div);
+                    const textInfo = text;
+                    div.insertAdjacentHTML("beforeend", textInfo);
+                    return div;
+                };
+                legend.addTo(this._map);
+            },
+            addCircle: function ({ accuracy, latitude, longitude }) {
+                return L.circle([latitude, longitude], accuracy / 2, {
+                    className: "circle-test",
+                    weight: 2,
+                    stroke: false,
+                    fillColor: "#136aec",
+                    fillOpacity: 0.15,
+                });
+            },
+            addMarker: function ({ latitude, longitude }) {
+                return L.marker([latitude, longitude], {
+                    icon: L.divIcon({
+                        className: styles.locatedAnimation,
+                        iconSize: L.point(17, 17),
+                        popupAnchor: [0, -15],
+                    }),
+                }).bindPopup("Your are here :)");
+            },
+            removeLocate: function () {
+                this._map.eachLayer(function (layer) {
+                    if (layer instanceof L.Marker) {
+                        const { icon } = layer.options;
+                        if (icon?.options.className === styles.locatedAnimation) {
+                            map.removeLayer(layer);
+                        }
+                    }
+                    if (layer instanceof L.Circle) {
+                        if (layer.options.className === "circle-test") {
+                            map.removeLayer(layer);
+                        }
+                    }
+                });
+            },
+        });
+
+        // adding new button to map controll
+        map.addControl(new customControl());
     }, [map]);
-  
+
     return null;
-  };
+};
+
+
+function DataBuilder(props) {
+    var dataArray = []
+    props.forEach(element => {
+        let pricetag = ''
+        if (element.U91 !== null && element.U91) { pricetag += 'Unleaded 91: ' + element.U91 }
+        if (element.E10 !== null && element.E10) { pricetag += '\nEthanol 10:' + element.E10 }
+        if (element.P95 !== null && element.P95) { pricetag += '\nPremium Unleaded 95: ' + element.P95 }
+        if (element.P98 !== null && element.P98) { pricetag += '\nPremium Unleaded 98: ' + element.P98 }
+        pricetag += '\n';
+        if (element.DL !== null && element.DL) { pricetag += '\nDiesel: ' + element.DL }
+        if (element.PDL !== null && element.PDL) { pricetag += '\nPremium Diesel: ' + element.PDL }
+        if (element.B20 !== null && element.B20) { pricetag += '\nBioDiesel 20: ' + element.B20 }
+        pricetag += '\n'
+        if (element.LPG !== null && element.LPG) { pricetag += '\nLPG: ' + element.LPG }
+        dataArray.push({ brand: element.brand, title: element.name, lat: element.loc_lat, lng: element.loc_lng, address: element.address, priceInfo: pricetag, price1: (''+element.U91+element.E10+element.P95+element.P98+element.DL+element.PDL+ element.LPG).replaceAll('undefined', '') })
+    });
+    return (dataArray);
+}
+
