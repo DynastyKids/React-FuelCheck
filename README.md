e26fee7ae79aeeb334341d6ba5aaca6a3db6a64a
DynastyKids(づ｡◕‿‿◕｡)づ <shabibox@outlook.com>
2022-04-13 02:08:29 +1000

    Patching the full address on UI for outside NSW

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ This is an unofficial web app, and is in no way connected with the any state Gov
  - Adding fuel type filter on navigation bar
  - Adding some stations for State Gov API unavailable area (VIC, QLD and ACT) through 3rd sources.
  - Adding 91 Opal (Low Aromatic Fuel) display support
+ - Adding navigate with Google Map option in popup window
 
 ## Future developments & Notices
 1. ~~ Adding fuel type filter for selection~~
--- src/components/body/osm_markers.js
+++ src/components/body/osm_markers.js
@@ -26,18 +26,18 @@ export default function OsmMarkers(props) {
             const fuelName=[["U91","E10","P95","P98","DL","PDL","B20","LPG","LAF"],["Unleaded 91","Ethanol 10","Premium Unleaded 95","Premium Unleaded 98","Diesel","Premium Diesel","BioDiesel","LPG","Low Aromatic Fuel"]]
             var index=0;
             for (index = 0; index < fuelName[0].length; index++) {
-                if (window.location.hash.substring(1) == fuelName[0][index]){
+                if (window.location.hash.substring(1) === fuelName[0][index]){
                     break;
                 }
             }
             props.jsondata.forEach(element => {
                 if (element[window.location.hash.substring(1)] !== null && element[window.location.hash.substring(1)]) {
-                    newdata.push({ "price1": element[window.location.hash.substring(1)], "address": element.address, "brand": element.brand, "loc_lat": element.loc_lat, "loc_lng": element.loc_lng, "name": element.name, "priceInfo": fuelName[1][index] + ": " + element[window.location.hash.substring(1)] })
+                    newdata.push({ "price1": element[window.location.hash.substring(1)], "address": element.address,"suburb": element.suburb,"state": element.state,"postcode": element.postcode, "brand": element.brand, "loc_lat": element.loc_lat, "loc_lng": element.loc_lng, "name": element.name, "priceInfo": fuelName[1][index] + ": " + element[window.location.hash.substring(1)] })
                     cheap5.push(element[window.location.hash.substring(1)]*100)
                 }
             });
             for (let index = 0; index < cheap5.length; index++) {
-                if(cheap5[index] === NaN){
+                if(isNaN(cheap5[index])){
                     console.log(index)
                 } ;
             }
@@ -47,7 +47,7 @@ export default function OsmMarkers(props) {
             props.jsondata.forEach(element => {
                 let priceInfo = ''
                 if (element.U91 !== null && element.U91) { priceInfo += 'Unleaded 91: ' + element.U91 }
-                if (element.LAF !== null && element.LAF) { priceInfo += 'Opal 91 (LAF): ' + element.LAF }
+                if (element.LAF !== null && element.LAF) { priceInfo += 'Low Aromatic Fuel (Opal 91): ' + element.LAF }
                 if (element.E10 !== null && element.E10) { priceInfo += '\nEthanol 94 (E10):' + element.E10 }
                 if (element.P95 !== null && element.P95) { priceInfo += '\nPremium Unleaded 95: ' + element.P95 }
                 if (element.P98 !== null && element.P98) { priceInfo += '\nPremium Unleaded 98: ' + element.P98 }
@@ -57,7 +57,7 @@ export default function OsmMarkers(props) {
                 if (element.B20 !== null && element.B20) { priceInfo += '\nBioDiesel 20: ' + element.B20 }
                 priceInfo += '\n'
                 if (element.LPG !== null && element.LPG) { priceInfo += '\nLPG: ' + element.LPG }
-                newdata.push({ "address": element.address, "brand": element.brand, "loc_lat": element.loc_lat, "loc_lng": element.loc_lng, "name": element.name, "priceInfo": priceInfo })
+                newdata.push({ "address": element.address, "suburb": element.suburb,"state": element.state,"postcode": element.postcode,"brand": element.brand, "loc_lat": element.loc_lat, "loc_lng": element.loc_lng, "name": element.name, "priceInfo": priceInfo })
             });
         }
         setFilteredData(newdata)
@@ -78,13 +78,15 @@ export default function OsmMarkers(props) {
     return (
         <>
             
-            {filteredData.map(({ brand, name, loc_lat, loc_lng, address, priceInfo, price1 }, index) => (
+            {filteredData.map(({ brand, name, loc_lat, loc_lng, address, suburb,state, postcode, priceInfo, price1 }, index) => (
                 <Marker key={index} position={[loc_lat, loc_lng]}>
                     {window.location.hash.length > 1 ? <Tooltip direction="top" opacity={1} permanent>{price1}</Tooltip> : <></>}
                     <Popup><h5>{name}</h5>
-                        <p><b>Addr:</b> {address}</p>
+                        <p><b>Address:</b> {address} {suburb!= null && suburb ? ", "+suburb : ""} {state!= null && state ? ", "+state : ""}{postcode!= null && postcode ? " "+postcode : ""}</p>
                         <div style={DisplayLineBreak}>
                             {priceInfo}
+                            <br/>
+                            <a href={"https://www.google.com/maps/search/?api=1&query="+loc_lat.toString()+","+loc_lng.toString()}>View in GoogleMap</a>
                         </div>
                     </Popup>
                 </Marker>
